f675f588746251a3b85b8ae5a408dd09be41ce59
vvhg1 <victor.hobe.gelting@gmail.com>
2023-12-26 13:06:14 +0100

    leftover from upstream merge

---
+++ build-info.h
@@ -0,0 +1,9 @@
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+#define BUILD_NUMBER 1398
+#define BUILD_COMMIT "ee652b2"
+#define BUILD_COMPILER "cc (Debian 10.2.1-6) 10.2.1 20210110"
+#define BUILD_TARGET "x86_64-linux-gnu"
+
+#endif // BUILD_INFO_H
